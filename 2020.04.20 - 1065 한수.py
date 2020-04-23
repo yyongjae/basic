76cdb11175188604681e9e65b0c2fea3b525fcee
dydwo706 <55885072+dydwo706@users.noreply.github.com>
2020-04-23 23:00:14 +0900

    Update and rename 2020.04.20 - 1065 한수.py 타임리밋 to 2020.04.20 - 1065 한수.py

--- 2020.04.20 - 1065 한수.py
+++ 2020.04.20 - 1065 한수.py
@@ -1,4 +1,4 @@
-N=int(input())
+N=(int)(input())
 cnt=0
 if N==1000:
     N-=1
@@ -10,20 +10,18 @@ else :
     cnt=99
     while N>99:
         n=N
-      
+        
         il=n%10
         n//=10
-      
+        
         sip=n%10
         n//=10
-       
+         
         baek=n%10
-       
+        
         if il-sip==sip-baek:
             cnt+=1
-            N-=1
+        N-=1
             
         
 print('%d' %cnt)
-        
-        
